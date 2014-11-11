d88a330a186e6cc82749137e4deb601d1857916e
seethakris <seethakris@gmail.com>
2014-11-11 17:20:59 +0800

    Create textfile for individual stimulus folders

---
+++ Python/create_textfile_for_thunder.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+"""
+Convert data to text from multi tiffs
+"""
+
+#Import relevant libraries
+import numpy as np #for numerical operations on arrays
+import PIL as pil # for image resizing
+import os  
+
+import matplotlib.pyplot as plt #for plotting
+from matplotlib.backends.backend_pdf import PdfPages
+import seaborn as sns
+
+from libtiff import TIFF #for reading multiTiffs
+
+#Custom libraries
+from smooth import smooth
+
+smooth_window = 5
+
+def create_textfile(Exp_Folder, filename_save_prefix, img_size_x, img_size_y,\
+            num_time, time_start, time_end, num_z_planes, stim_start, stim_end,\
+            f_f_flag, dff_start, dff_end):
+    
+    filesep = os.path.sep #Get fileseperator according to operating system
+    
+    # To plot as PDF create directory
+    Figure_PDFDirectory = Exp_Folder+filesep+'Figures'+filesep
+    if not os.path.exists(Figure_PDFDirectory):
+        os.makedirs(Figure_PDFDirectory) 
+    pp = PdfPages(Figure_PDFDirectory+filename_save_prefix+'_PreprossData.pdf')
+    
+    zz = 0 #each multitiff file is considered as one stack
+    Matfile_for_Thunder = None
+    
+    #Find tiff files in given experiment folder 
+    onlyfiles = [ f for f in os.listdir(Exp_Folder) if (os.path.isfile(os.path.join(Exp_Folder,f)) and f.find('.tif')>0 and f.find('Registered_Z=')==0)]
+        
+    #Get data from multitiffs. - Conditions to check while loading data - num_z_planes and time_start and time_end
+    if num_z_planes!=0: # Get only planes specified in the array num_z_planess
+        
+        for lst in xrange(0,np.size(num_z_planes, axis=0)): 
+            tif = TIFF.open(os.path.join(Exp_Folder,('Registered_Z='+str(num_z_planes[lst])+'.tif')), mode='r') #Open multitiff 
+            
+            zz = zz+1  #Update the number of current z plane to add to text file
+            
+            # Get image data as a matrix. Also Plot average image for validation
+            data = get_data_from_tiff(tif, img_size_x, img_size_y, num_time, num_z_planes[lst], pp) #get data in matrix form from multitiff
+            
+            #Get data in thunder format [xx,yy,zz,time]
+            temp_matfile_for_thunder = get_matrix_for_textfile(data, zz, time_start, time_end,\
+            f_f_flag, dff_start, dff_end, stim_start,stim_end, num_z_planes[lst], pp)
+            
+            #Append each tiff files data to a bigger matrix and Plot heatmap of data for validation
+            if Matfile_for_Thunder == None:
+                Matfile_for_Thunder = temp_matfile_for_thunder
+            else:
+                Matfile_for_Thunder = np.append(Matfile_for_Thunder,temp_matfile_for_thunder, axis=0)
+            
+    else: #Get all files
+        
+        for lst in xrange(1,np.size(onlyfiles, axis=0)+1): 
+            tif = TIFF.open(os.path.join(Exp_Folder,('Registered_Z='+str(lst)+'.tif')), mode='r') #Open multitiff  
+            
+            zz = zz+1 #Update the number of current z plane to add to text file
+            
+            # Get image data as a matrix
+            data = get_data_from_tiff(tif, img_size_x, img_size_y, num_time, lst, pp) #get data in matrix form from multitiff
+            
+            #Get data in thunder format [xx,yy,zz,time]
+            temp_matfile_for_thunder = get_matrix_for_textfile(data, zz, time_start, time_end,\
+            f_f_flag, dff_start, dff_end, stim_start,stim_end, lst, pp)
+            
+            #Append each tiff files data to a bigger matrix
+            if Matfile_for_Thunder == None:
+                Matfile_for_Thunder = temp_matfile_for_thunder
+            else:
+                Matfile_for_Thunder = np.append(Matfile_for_Thunder,temp_matfile_for_thunder, axis=0)
+            
+    pp.close()        
+    print 'Saving all the data in the text file '            
+    np.savetxt(Exp_Folder+filesep+filename_save_prefix+'.txt', Matfile_for_Thunder, fmt='%i')#Save as text file
+    
+    return Matfile_for_Thunder
+    
+    
+def get_data_from_tiff(tif, img_size_x, img_size_y, num_time, filename, pp):
+    data = np.zeros((img_size_x,img_size_y,num_time), dtype=np.uint8)
+    ii = 0
+    for image in tif.iter_images():
+        #Check if image is of the xy resolution specified, else resize
+        if np.size(image,1)!=img_size_y or np.size(image,0)!=img_size_x:
+            if ii == 0: #Print that there is a size mismatch
+                print 'Size mismatch..Resizing Z='+ str(filename)
+            temp_image = pil.Image.fromarray(image)    
+            data[:,:,ii] = np.array(temp_image.resize((img_size_y, img_size_x), pil.Image.NEAREST))            
+            ii = ii+1
+        else:
+            data[:,:,ii] = image
+            ii = ii+1   
+    #Plot average data over time for reviewingin grayscale      
+    with sns.axes_style("white"):
+        fig1 = plt.imshow(np.mean(data[:,:,:], axis=2), cmap='gray')
+        plt.title('Z='+str(filename))
+        plt.axis('off')
+        fig1 = plt.gcf()
+        pp.savefig(fig1)
+        plt.close()
+        
+    return data
+
+def get_matrix_for_textfile(data, zz, time_start, time_end,f_f_flag, dff_start, dff_end,stim_start,stim_end,filename, pp):
+    
+    print 'Creating array from stack for Z=' + str(filename)
+    temp_matfile_for_thunder = np.zeros([np.size(data, axis=0)*np.size(data, axis=1),3+(time_end-time_start+1)+smooth_window-2], dtype=np.int)
+    count = 0    
+    for yy in xrange(0,np.size(data, axis=1)):
+        for xx in xrange(0,np.size(data, axis=0)): 
+            temp_matfile_for_thunder[count,0] = xx+1;
+            temp_matfile_for_thunder[count,1] = yy+1;
+            temp_matfile_for_thunder[count,2] = zz;
+            # Create delta f/f values if necessary
+            if f_f_flag==0:
+                temp_matfile_for_thunder[count,3:] = smooth(data[xx,yy,time_start:time_end],smooth_window,'hanning')
+            else:
+                temp_matfile_for_thunder[count,3:] = smooth(((data[xx,yy,time_start:time_end]-np.mean(data[xx,yy,dff_start:dff_end]))/np.std(data[xx,yy,dff_start:dff_end])),smooth_window,'hanning')
+            count = count+1 
+    
+    #Plot heatmap for validation    
+    with sns.axes_style("white"):
+        A = temp_matfile_for_thunder[:,3:]    
+        B = np.argsort(np.mean(A, axis=1))  
+        C = A[B,:]
+        if f_f_flag == 1: #Plot with correct climif dff is true
+            fig2 = plt.imshow(C[-1000:,:],aspect='auto', cmap='jet',vmin=-5, vmax=5)
+        else:
+            fig2 = plt.imshow(C[-1000:,:],aspect='auto', cmap='jet')
+        plot_vertical_lines(stim_start-time_start,stim_end-time_start)
+        labels, locs = plt.xticks()
+        labels1 = [int(item) for item in labels]
+        labels2 = [str(int(item)+time_start) for item in labels]
+        plt.xticks((labels1),(labels2))
+        plt.xlim(0,(time_end-time_start))
+        plt.title('Sorted Heatmap Z='+str(filename))
+        plt.colorbar()
+        fig2 = plt.gcf()
+        pp.savefig(fig2)
+        plt.close()
+        A = None    
+
+    return temp_matfile_for_thunder[:,-smooth_window+1:]
+
+def plot_vertical_lines(stim_start,stim_end):
+    plt.axvline(x=stim_start, linestyle='-', color='k', linewidth=1)
+    plt.axvline(x=stim_end, linestyle='--', color='k', linewidth=1)
+    
