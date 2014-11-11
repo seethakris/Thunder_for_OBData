0e643a6792fe090659f7b321165e45b3a2a151a1
seethakris <seethakris@gmail.com>
2014-11-11 19:02:40 +0800

    Now creates text file for individual files and all files combined

--- Python/run_thunder.py
+++ Python/run_thunder.py
@@ -14,17 +14,17 @@ Exp_Folder = '/Users/seetha/Desktop/Michelle_OB_Thunder/Data/141010 Fish2 Deconv
 
 #Prefix using which all text files, figures, matfiles and numpy array for this run of thunder will be saved. 
 #If text file with prefixed name already exists, the script will go straight to running PCA 
-filename_save_prefix = 'test1' 
+filename_save_prefix = 'test_ind' 
 
 #Experiment parameters
-img_size_x = 384 #X and Y resolution - if there are images that dont have this resolution, they will be resized
-img_size_y = 502
+img_size_x = 20 #384 #X and Y resolution - if there are images that dont have this resolution, they will be resized
+img_size_y = 20 #502
 num_time = 121  #Total Number of time points in experiment
-num_z_planes = [1,4,5] #Mention z-planes to be used for analysis. If all planes to be used, say num_z_planes=0
+num_z_planes = 0 #Mention z-planes to be used for analysis. If all planes to be used, say num_z_planes=0
 
 #Only time points specified in these two variables will be used 
 #for creating text files and further analysis
-time_start = 0 #Starting time point 
+time_start = 1 #Starting time point 
 time_end = 121 #Ending time point
 
 #Stimulus on and off time
@@ -32,12 +32,12 @@ stim_start = 50 #Stimulus Starting time point
 stim_end = 60 #Stimulus Ending time point
 
 #Set if you want to use raw images or delta f/f. If delta f/f is needed, specify baseline time points
-f_f_flag =  0#0-raw data, 1-delta f/f
+f_f_flag =  0 #0-raw data, 1-delta f/f
 dff_start = 1
 dff_end = 20
 
 #Stimulus Parameters
-combine = 0 #1 - Combine planes from all stimulus folders in Exp_Folder, 0-Run PCA on each seperately
+combine = 0 #1 - Combine time points from all stimulus planes in Exp_Folder, 0-Run PCA on each seperately
 
 #PCA parameters
 pca_components = 3 #Number of pca components to detect from files
@@ -60,7 +60,7 @@ from thunder import ThunderContext
 
 #Import user defined libraries
 from create_textfile_for_thunder import create_textfile
-from create_textfile_for_thunder import create_textfile_combined
+from create_textfile_for_thunder_combined import create_textfile_combined
 
 ######################################################################
 
@@ -72,7 +72,7 @@ from create_textfile_for_thunder import create_textfile_combined
 
 #Check appropriate folders if combine = 0 or 1
 #Find Stimulus folder
-Stimulus_Folders = [f for f in os.listdir(Exp_Folder) if os.path.isdir(os.path.join(Exp_Folder, f))] 
+Stimulus_Folders = [f for f in os.listdir(Exp_Folder) if os.path.isdir(os.path.join(Exp_Folder, f)) and f.find('Figures')<0] 
 
 if combine == 0: #check for text file in individual folders   
     #Check if text file already exists in each stimulus folder, else create it
@@ -95,7 +95,7 @@ else:#check for text file in top folder
     if len(txt_file)==0: 
         start_time = time.time() 
         print 'Saving images to text on all files in '+ Exp_Folder
-        Matfile_for_thunder = create_textfile_combined(Exp_Folder, filename_save_prefix, img_size_x, img_size_y,\
+        Matfile_for_thunder = create_textfile_combined(Exp_Folder, Stimulus_Folders, filename_save_prefix, img_size_x, img_size_y,\
         num_time, time_start, time_end, num_z_planes, stim_start, stim_end, f_f_flag, dff_start, dff_end) #Create text file
         print 'Saving to text file took '+ str(int(time.time()-start_time)) +' seconds'
 
